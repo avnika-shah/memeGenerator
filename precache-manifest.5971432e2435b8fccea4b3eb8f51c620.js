369606fd3995544340c03006dd0ef223fe37a3eb
avnika <champ_avnika@hotmail.com>
2020-07-04 11:37:22 +0530

    Updates

---
+++ precache-manifest.5971432e2435b8fccea4b3eb8f51c620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c44d25941a4aeb10a216336aaddb286",
+    "url": "/memeGenerator/index.html"
+  },
+  {
+    "revision": "74de707e8723ddb9485b",
+    "url": "/memeGenerator/static/js/2.57248dc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memeGenerator/static/js/2.57248dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979ed2de858b8945fe72",
+    "url": "/memeGenerator/static/js/main.74a1e7d8.chunk.js"
+  },
+  {
+    "revision": "43f0cde6ba15432d232f",
+    "url": "/memeGenerator/static/js/runtime-main.574b2b5f.js"
+  }
+]);
